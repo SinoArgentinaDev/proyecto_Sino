52476375a49e076d0f9dc01561b85e15ca1f44b0
AngieDev21 <a.cohen.academit@gmail.com>
2025-01-23 16:32:15 -0300

    [add] se agregaron estructuras a la pagina Nosotros

---
+++ src/components/Titulo/Titulo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Titulo = ({title, title2}) => {
+  return (
+    <div className='mx-auto mt-16 px-4 sm:px-6 lg:px-8 w-full max-w-8xl'> {/* Contenedor responsivo */}
+      <h1 
+        className='text-3xl sm:text-4xl lg:text-5xl font-medium text-center tracking-wide text-[#051d40] leading-snug sm:leading-tight'>
+       {title}<br />
+        <span className='block'>{title2}</span>
+      </h1>
+    </div>
+  );
+};
