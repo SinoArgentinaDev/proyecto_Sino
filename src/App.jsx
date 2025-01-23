fe928550151b5e558db3ff19815f29b56aa723fe
AngieDev21 <a.cohen.academit@gmail.com>
2025-01-23 13:58:50 -0300

    [add] se inicia la creacion de la pagina Sobre Nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Home} from "./pages/Home";
-import { Sidebar } from "./components/Siderbar/Sidebar";
+import { Nosotros } from "./pages/Nosotros";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/side" element={<Sidebar />} />
+        <Route path="/nosotros" element={<Nosotros/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Nosotros.jsx
@@ -0,0 +1,9 @@
+import {Header} from '../components/shared/Header/Header'
+
+export const Nosotros = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
