30c74cfe1648d7b1d894a824088cd5e0e074f778
AngieCohenDev <angiedev2001@gmail.com>
2025-02-18 15:56:49 -0300

    FIX. se cambio el texto para la seccion de sobre nosotros

--- src/components/Portada/Video.jsx
+++ src/components/Portada/Video.jsx
@@ -21,7 +21,9 @@ export const Video = () => {
       {/* Contenedor para el texto y el botón */}
       <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white">
         {/* Etiqueta H1 centrada */}
-        <h1 className="text-[70px] font-bold tracking-wide mt-24 text-center">
+        <h1 className="text-[70px] font-bold tracking-wide mt-24 text-center text-white 
+               [text-shadow:_1px_1px_0px_black,-1px_-1px_0px_black,1px_-1px_0px_black,-1px_2px_0px_black]
+               [-webkit-text-stroke:1px_black]">
           {t("cover.main-title")}
         </h1>
 
