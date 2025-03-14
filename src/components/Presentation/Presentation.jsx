64ad732c0f7e99d7784c4a98d1aea9e7ae329fc4
AngieDev21 <a.cohen.academit@gmail.com>
2025-03-14 14:08:03 -0300

    [fix] correcion de textos

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useTranslation} from "react-i18next"
 import { FaAsterisk } from "react-icons/fa";
 import { CardPresentation } from "./CardPresentation";
 
@@ -7,10 +8,11 @@ import lorenzo from "../../assets/img/lorenzo.png";
 import quinto from "../../assets/img/quinto.jpg";
 
 export const Presentation = () => {
+   const [t, i18n] = useTranslation("global")
   return (
     <section className="relative px-6 sm:px-24 lg:px-40 py-12 bg-gray-100">
       <h1 className="text-4xl font-extrabold text-center tracking-wider text-[#051d40] mt-6 mb-12">
-        PRESENTANDO A NUESTRO EQUIPO
+      {t("presentation.title")}
       </h1>
 
       <FaAsterisk className=" hidden md:block  absolute left-0 top-1/2 transform -translate-y-1/2 text-[#051d40] text-4xl mx-6" />
@@ -18,21 +20,21 @@ export const Presentation = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8">
         <CardPresentation
           img={marcos}
-          title="Marcos Puelles Lanús"
-          subtitle="Socio fundador | Gerente general"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum eros sit amet nibh ornare tempus. In ut mauris scelerisque, sodales leo vehicula."
+          title={t("presentation.title_1")}
+          subtitle={t("presentation.subtitle_1")}
+          description={t("presentation.description_1")}
         />
         <CardPresentation
           img={quinto}
-          title="Quinto Scuticchio"
-          subtitle="Socio fundador | Consultor de negocios"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum eros sit amet nibh ornare tempus. In ut mauris scelerisque, sodales leo vehicula."
+          title={t("presentation.title_2")}
+          subtitle={t("presentation.subtitle_2")}
+          description={t("presentation.description_2")}
         />
         <CardPresentation
           img={lorenzo}
-          title="Fernando Lorenzo"
-          subtitle="Socio fundador | Dpto. Comercial"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum eros sit amet nibh ornare tempus. In ut mauris scelerisque, sodales leo vehicula."
+          title={t("presentation.title_3")}
+          subtitle={t("presentation.subtitle_3")}
+          description={t("presentation.description_3")}
         />
       </div>
 
