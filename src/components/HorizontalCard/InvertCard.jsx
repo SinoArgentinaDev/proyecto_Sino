eb713c309f89ea357d6c0d4265991253df290841
AngieDev21 <a.cohen.academit@gmail.com>
2025-08-04 18:20:40 -0300

    correcion imagenes

--- src/components/HorizontalCard/InvertCard.jsx
+++ src/components/HorizontalCard/InvertCard.jsx
@@ -5,7 +5,7 @@ export const InvertCard = ({ url, title, texts }) => {
     <div className="flex flex-col md:flex-row items-center md:items-start gap-10 md:gap-12 p-8 md:p-12 lg:p-16 bg-white ">
       {/* Image Section */}
       <div
-         className="relative w-full max-w-full md:w-[550px] md:h-[200px] lg:h-[60vh] overflow-hidden"
+         className="relative w-full max-w-full md:w-[550px] md:h-[340px] lg:h-[60vh] overflow-hidden"
         style={{
           clipPath: 'polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%)',
         }}
