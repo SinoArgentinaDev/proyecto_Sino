47c281590c8d220c22c8737c8d4194abdd09c230
AngieDev21 <a.cohen.academit@gmail.com>
2025-08-04 18:26:46 -0300

    cambios

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -7,7 +7,7 @@ import { Contacto } from '../components/Contacto/Contacto';
 import nosotrosUno from '../assets/videos/dos.mp4';
 import nosotros from '../assets/videos/ntres.mp4';
 import mision from '../assets/img/homeImg.png';
-import vision from '../assets/img/sino2.jpg';
+import vision from '../assets/img/Sino2.jpg';
 import { HorizontalCard } from '../components/HorizontalCard/HorizontalCard';
 import { InvertCard } from '../components/HorizontalCard/InvertCard';
 import { WhyUs } from '../components/WhyUs/WhyUs';
